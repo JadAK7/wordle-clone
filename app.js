3f5be9d375eb2c16e69271407706cb596e1af206
Jad Abou Karam <jadaboukaram2002@hotmail.com>
2022-04-22 16:35:16 +0300

    starter 2

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.static("styling"));
+app.use(express.static("js"));
+
+app.get("/normal-game", (req, res) => {
+  res.render("normal-game");
+});
 
 app.get("/", (req, res) => {
   res.render("index");
